c481842caf673baca4954920ae0fae6bf2f37ae3
Alvaro <alvaro_gonzalez_tsapp1ob2021@cev.com>
2024-02-26 17:48:26 +0100

    navbar component&dark/light theme from shadcn-ui

---
+++ data.tsx
@@ -0,0 +1,29 @@
+import { BellPlus, BookText, Briefcase, Brush, Code2, CodeSquare, Home, Inbox, Mail, PanelsTopLeft, Phone, UserRound, UsersRound, Wrench,Presentation,Handshake, Send } from "lucide-react"; 
+
+export const navBarElements = [
+    {
+        id: 1,
+        title: "Home",
+        icon: <Home size={20} />,
+        link: "#home",
+    },
+    {
+        id: 2,
+        title: "About me",
+        icon: <Presentation size={20} />,
+        link: "#about",
+    },
+    {
+        id: 3,
+        title: "Services",
+        icon: <Handshake size={20} />,
+        link: "#services",
+    },
+    {
+        id: 4,
+        title: "Contact me",
+        icon: <Send size={20} />,
+        link: "#contact",
+    },
+   
+];
